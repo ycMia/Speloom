4a8d2ab93ac4d7f37a2916a4416f5c6c9ed74cdc
ycMia <zzlyx20123201@126.com>
2023-11-27 01:28:24 +0800

    Mon Nov 27 01:28:24 CST 2023

---
+++ js/inspireBallAnimation.js
@@ -0,0 +1,16 @@
+window.onload = function () {
+    insBallHandler = document.getElementsByClassName("inspire-ball")[0];
+//  alert(insBallHandler);
+    insBallHandler.onanimationend = function () {
+        if(insBallHandler.classList.contains("inspire-ball-ani-1"))
+        {
+            insBallHandler.classList.remove("inspire-ball-ani-1");
+            insBallHandler.classList.add("inspire-ball-ani-2");
+        }
+        else
+        {
+            insBallHandler.classList.remove("inspire-ball-ani-2");
+            insBallHandler.classList.add("inspire-ball-ani-1");
+        }
+    }
+}
